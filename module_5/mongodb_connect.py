ad28cf78ce3238c156fc808dd90d814bf7caf012
atlantisfan <73559944+atlantisfan@users.noreply.github.com>
2021-02-07 21:26:33 -0500

    mongodb_connect.py

---
+++ module_5/mongodb_connect.py
@@ -0,0 +1,7 @@
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.52yzh.mongodb.net/pytech?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
+
