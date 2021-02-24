a63516bd8f2ed37efb6ba4cbf06c7c3fc359daa0
atlantisfan <73559944+atlantisfan@users.noreply.github.com>
2021-02-23 22:40:53 -0500

    pysports_join_queries.py

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -1,57 +1,31 @@
-""" 
-    Title: pysports_join_queries.py
-    Author: Professor Krasso
-    Date: 16 July 2020
-    Description: Test program for joining the player and team tables
-"""
+# Justin Brehms
+# CSD 310 Databse design
+# Module 9.2
 
-""" import statements """
 import mysql.connector
-from mysql.connector import errorcode
+# Login to mysql database using pysports_user
+pysports = mysql.connector.connect (
+    host="127.0.0.1",
+    user="pysports_user",
+    password="MySQL8IsGreat!",
+    database="pysports"
+    )
 
-""" database config object """
-config = {
-    "user": "pysports_user",
-    "password": "MySQL8IsGreat!",
-    "host": "127.0.0.1",
-    "database": "pysports",
-    "raise_on_warnings": True
-}
+mycursor = pysports.cursor()
 
-try:
-    """ try/catch block for handling potential MySQL database errors """ 
+# perform inner join 
+cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
 
-    db = mysql.connector.connect(**config) # connect to the pysports database 
+# get results from cursor 
+players = cursor.fetchall()
 
-    cursor = db.cursor()
+print("\n  -- DISPLAYING PLAYER RECORDS --")
 
-    # inner join query 
-    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
+# use for loop to iterate results 
+for player in players:
+    print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
-    # get the results from the cursor object 
-    players = cursor.fetchall()
+input("\n\n  Press any key to continue... ")
 
-    print("\n  -- DISPLAYING PLAYER RECORDS --")
-    
-    # iterate over the player data set and display the results 
-    for player in players:
-        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
-
-    input("\n\n  Press any key to continue... ")
-
-except mysql.connector.Error as err:
-    """ handle errors """ 
-
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("  The supplied username or password are invalid")
-
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("  The specified database does not exist")
-
-    else:
-        print(err)
-
-finally:
-    """ close the connection to MySQL """
-
-    db.close()
+#close connection to database
+db.close()
